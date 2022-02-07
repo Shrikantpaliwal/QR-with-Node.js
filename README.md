9ad991cdec82d7b8ebacbe7f06198b3aa90d404d
shrikantpaliwal <shrikantpaliwal001@gmail.com>
2022-02-08 04:49:22 +0530

    Fixed Client server comm.

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ Simple QR Code Generator with Node.js (A JavaScript Runtime Environment)
 
 ### First Hour Progress
 ![alt text](doc/img1.png)
+
+
+## Day 2 One Hour Progress
+![alt text](doc/img2.png)
--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 var path = require("path");
-var QRCode = require('qrcode')
+var QRCode = require('qrcode');
+const { type } = require('os');
 
 const app = express()
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname,'./public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, './public')));
 
+app.post('/generate', (req, res) => {
+    var rec_url = String(req.query.url);
+    console.log(req.params['url'])
+    QRCode.toString("google.com", {type:'svg'}, (err, qr_str) => {
+        if (err) {
+            console.clear()
+            console.log(err)
+        }
+        console.clear()
+        console.log(qr_str)
+        res.send(qr_str)
+    })
+})
 
-
-app.get('/', function (req, res){
-    res.sendFile(path.join(__dirname,'./public/index.html'));
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(8091)
